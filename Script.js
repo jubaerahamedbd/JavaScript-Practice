32ec2154fa9019ede680a62aa1522c34de73f57a
jubaerahamedbd <jubaerahamed@gmail.com>
2021-10-12 23:15:13 +0600

    Value

--- Script.js
+++ Script.js
@@ -7,9 +7,15 @@
 // console.log(para.textContent) //show all hidden space and elements also
 // console.log(para.innerText) //show text only
 
-// Style
+// // Style
 
-const para = document.querySelector('p')
+// const para = document.querySelector('p')
 
-para.style.backgroundColor = 'black' //add all css property as far need
-para.style.color = 'white'
+// para.style.backgroundColor = 'black' //add all css property as far need
+// para.style.color = 'white'
+
+// Value
+const forms = document.querySelectorAll('input')
+
+console.log(forms[0].value) //print or use input text value
+console.log(forms[3].checked) // checked used for checkbox input
