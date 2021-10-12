85a78d1cfe76093688868d008b59771a8dbfff62
jubaerahamedbd <jubaerahamed@gmail.com>
2021-10-12 23:07:13 +0600

    Style

--- Script.js
+++ Script.js
@@ -1,8 +1,15 @@
-//Inner text
+// //Inner text
+// const para = document.querySelector('p')
+// const paraAll = document.querySelectorAll('p') //select all paragraph item
+
+// para.innerText = 'Changed Text' //change text
+
+// console.log(para.textContent) //show all hidden space and elements also
+// console.log(para.innerText) //show text only
+
+// Style
+
 const para = document.querySelector('p')
-const paraAll = document.querySelectorAll('p') //select all paragraph item
 
-para.innerText = 'Changed Text' //change text
-
-console.log(para.textContent) //show all hidden space and elements also
-console.log(para.innerText) //show text only
+para.style.backgroundColor = 'black' //add all css property as far need
+para.style.color = 'white'
